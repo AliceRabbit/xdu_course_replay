38d99be69a8bc7198aaaf2233a7e174f31074f0f
compass <290626396@qq.com>
2024-12-09 20:23:07 +0800

    修正BUG

--- get_courses.py
+++ get_courses.py
@@ -1,16 +1,34 @@
 import requests
 from utils.config import load_config
 import json
+import datetime
 
 def fetch_courses():
-    url = "https://newesxidian.chaoxing.com/frontLive/listStudentCourseLivePage?fid=16820&userId=323992114&week=3&termYear=2024&termId=1&type=1"
-    headers = load_config("config.yaml").get("headers")
+    config = load_config("config.json")
+    if config is None:
+        return
+    headers = config.get("headers")
+    fid = config.get("fid")
+    userId = config.get("uId")
+    termYear = datetime.datetime.now().year
+    termId = get_current_termId()
+    week = 4
+    url = f"https://newesxidian.chaoxing.com/frontLive/listStudentCourseLivePage?fid={fid}&userId={userId}&week={week}&termYear={termYear}&termId={termId}&type=1"
+    # url = "https://newesxidian.chaoxing.com/frontLive/listStudentCourseLivePage?fid=16820&userId=323992114&week=3&termYear=2024&termId=1&type=1"
     response = requests.get(url, headers=headers)
     data = response.json()
     
     # 格式化输出 JSON 数据
     formatted_json = json.dumps(data, indent=4, ensure_ascii=False)
     print(formatted_json)
+
+def get_current_termId():
+    month = datetime.datetime.now().month
+
+    if 3 <= month <= 8:
+        return 2
+    else:
+        return 1
 
 def main():
     fetch_courses()
